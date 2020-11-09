6cfbaa6bab50a77f6839115b03fa2d78d1be387e
Lamanris <Dreamer19042002@gmail.com>
2020-11-09 17:43:35 +0600

    beginning

---
+++ slider/slider.js
@@ -0,0 +1,21 @@
+let fillingSwiper = new Swiper('.fillingSwiper-container', {
+    loop: true,
+    spaceBetween: 50,
+    navigation: {
+        nextEl:'.swiper-filling-button-next',
+        prevEl:'.swiper-filling-button-prev',
+    },
+
+});
+
+
+// $("#photoButton").on("click",function(){
+//     setTimeout(function () {
+//         photoSwiper1.update();
+//     }, 1);
+// });
+// $("#videoButton").on("click",function(){
+//     setTimeout(function () {
+//         videoSwiper1.update();
+//     }, 1);
+// });
