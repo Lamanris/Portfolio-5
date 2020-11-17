600de1bf8d2666441c688f51f0f713a8e9f93a90
Lamanris <Dreamer19042002@gmail.com>
2020-11-17 22:53:15 +0600

    added some sliders, section

--- slider/slider.js
+++ slider/slider.js
@@ -5,8 +5,52 @@ let fillingSwiper = new Swiper('.fillingSwiper-container', {
         nextEl:'.swiper-filling-button-next',
         prevEl:'.swiper-filling-button-prev',
     },
-
+    pagination: {
+        el: '.swiper-filling-pagination',
+        type: 'fraction',
+    },
 });
+if (screen.width <= 550) {
+    let sizingSwiper = new Swiper('.sizingSwiper-container', {
+        loop: true,
+        spaceBetween: 50,
+        pagination: {
+            el: '.swiper-sizing-pagination',
+        },
+    });
+}
+let lookingSwiper = new Swiper('.lookingSwiper-container', {
+    loop: true,
+    slidesPerView: 1,
+    centeredSlides: true,
+    navigation: {
+        nextEl:'.swiper-looking-button-next',
+        prevEl:'.swiper-looking-button-prev',
+    },
+    pagination: {
+        el: '.swiper-looking-pagination',
+        type: 'fraction',
+    },
+    breakpoints: {
+        700: {
+            slidesPerView: 2,
+            centeredSlides: false,
+        }
+    }
+});
+let reviewsSwiper = new Swiper('.reviewsSwiper-container', {
+    loop: true,
+    navigation: {
+        nextEl:'.swiper-reviews-button-next',
+        prevEl:'.swiper-reviews-button-prev',
+    },
+    pagination: {
+        el: '.swiper-reviews-pagination',
+        type: 'fraction',
+    },
+});
+
+
 
 
 // $("#photoButton").on("click",function(){
