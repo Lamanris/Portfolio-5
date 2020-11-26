fbcf422073c481ea1a995d144ed216b3d2daf268
Lamanris <Dreamer19042002@gmail.com>
2020-11-26 17:48:03 +0600

    some fixes with burger

--- js/script.js
+++ js/script.js
@@ -1,17 +1,20 @@
 document.querySelector('.header__burger').addEventListener ('click', () => {
     document.querySelector('.header__burger').classList.toggle('active');
     document.querySelector('.header__menu-burger').classList.toggle('active');
+    document.querySelector('.header__menu').classList.toggle('active');
     document.querySelector('body').classList.toggle('lock')
 });
 document.querySelectorAll('.header__menu__item').forEach((el) => {
     el.addEventListener('click', () => {
         document.querySelector('.header__menu-burger').classList.remove('active');
+        document.querySelector('.header__menu').classList.remove('active');
         document.querySelector('.header__burger').classList.remove('active');
         document.querySelector('body').classList.remove('lock');
     })
 });
 document.querySelector('.header__logo').addEventListener('click', () => {
     document.querySelector('.header__menu-burger').classList.remove('active');
+    document.querySelector('.header__menu').classList.remove('active');
     document.querySelector('.header__burger').classList.remove('active');
     document.querySelector('body').classList.remove('lock');
 });
